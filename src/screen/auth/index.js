a2a3cb2b98c03dcca9f9e29657f0a660bd20de5e
MinalGlobal <pmi.globaliasoft@gmail.com>
2022-06-01 15:58:53 +0530

    phone ios verification

--- src/screen/auth/index.js
+++ src/screen/auth/index.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage'
 import { TextInputMask } from 'react-native-masked-text'
+import auth from '@react-native-firebase/auth';
 
 //component
 import { styles } from './style'
@@ -15,6 +16,8 @@ import { signup, loginfirebase } from '../../utility/Firebase/auth'
 import { Loginuser } from '../../helper/authentication'
 import { Adduser } from '../../utility/redux/action/userData'
 import { add } from 'react-native-reanimated';
+import { TextInput } from 'react-native';
+import { Touchable } from 'react-native';
 
 const group = ["group1", "group2"]
 
@@ -37,7 +40,10 @@ const LoginScreen = (props) => {
     const [number, setNumber] = useState('')
     const [address, setAddress] = useState("")
     const [groupname, setGroupname] = useState("Group1")
+     //confirma
 
+     const [confirm , setConfirm] = useState()
+     const [value , setValue] = useState("")
 
 
     const changelogin = (value) => {
@@ -50,16 +56,36 @@ const LoginScreen = (props) => {
         }
     }
 
-    const handlelogin = () => { Loginuser(navigation, dispatch, username, password) }
 
+    const handlelogin = async () => {
+     //    Loginuser(navigation, dispatch, username, password)
+   //  let phoneNumber = 8154822559
+   let phoneNumber = 8238678399
+  
+     const confirmation = await auth().signInWithPhoneNumber("+91"+phoneNumber)
+     //console.log("%%%%%%%",confirmation.confirm(verifiy))
+     setConfirm(confirmation)
+    
+ }
 
+const veri = () =>{
+    confirm.confirm(value)
+    .then(user =>  console.log("&&&&&&&",user))
+    .catch(error => console.log("%%%%%",error))
+}
+        
     return (
         <View style={styles.container}>
             <Image source={require("../../../assets/images/logo.jpeg")}
                 style={styles.image} />
             
             <View style={{ flex: 1, width: "100%", height: "100%" }}>
-                
+                <TextInput placeholder = "value"
+                            value={value}
+                            onChangeText={(tex) =>setValue(tex)}/>
+                <TouchableOpacity onPress={() =>veri()}>
+                    <Text>varify</Text>
+                </TouchableOpacity>
                     <Login username={username}
                         setusername={setUsername}
                         setpassword={setPassword}
